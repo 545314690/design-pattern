4cf5e98f2ca86a4afd8e1ea1896d595248bf1865
lisenmiao <545314690@qq.com>
2015-11-26 14:40:21 +0800

    Initial commit

---
+++ src/main/java/org/m/parttern/test/Test.java
@@ -0,0 +1,10 @@
+package org.m.parttern.test;
+
+/**
+ * Created by lism on 2015/11/26.
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.print("This is a test");
+    }
+}
